1324f53b395451358a98c009a457141f9cb8a5a8
Jason Grafmiller (English Language and Linguistics) <j.grafmiller@bham.ac.uk>
2022-09-21 09:24:56 +0100

    rename function

--- R/marginal_avg.R
+++ R/marginal_avg.R
@@ -23,7 +23,7 @@
 #' @examples
 #' \dontrun{}
 #' @importFrom data.table ":="
-weighted_predictions <- function(marginal_preds, target.vars, equal.wt = NULL,
+avg_predictions <- function(marginal_preds, target.vars, equal.wt = NULL,
                          wt = c("iso", "joint", "all"),
                          interval = c(.05, .95),
                          verbose = TRUE){
